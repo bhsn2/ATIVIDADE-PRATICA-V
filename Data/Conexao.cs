04d372b6af231e254e08681e7ddeac2212ef7eaa
bhsn2 <bhsn@aluno.ifnmg.edu.br>
2026-06-28 20:57:01 -0300

    Add files via upload

---
+++ Data/Conexao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Estacionamento.Data
+{
+    public static class Conexao
+    {
+        private static readonly string _connectionString =
+            @"Server=(localdb)\MSSQLLocalDB;Database=Estacionamento;Integrated Security=True;";
+
+        public static SqlConnection Abrir()
+        {
+            try
+            {
+                SqlConnection conexao = new SqlConnection(_connectionString);
+                conexao.Open();
+                return conexao;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception($"Erro ao conectar ao banco de dados: {ex.Message}");
+            }
+        }
+
+        public static bool Testar()
+        {
+            try
+            {
+                using (SqlConnection conexao = Abrir())
+                {
+                    return conexao.State == System.Data.ConnectionState.Open;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
